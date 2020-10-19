2048f268794bb686044c3292b1be18b6c3d9905e
Chris <chriskirbydev@gmail.com>
2020-10-19 19:39:00 +1300

    Create components folder and landing page files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ShellModule } from './shell/shell.module';
 import { AboutModule } from './about/about.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   imports: [
@@ -32,7 +33,7 @@ import { AppRoutingModule } from './app-routing.module';
     AuthModule,
     AppRoutingModule, // must be imported as the last module as it contains the fallback route
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingPageComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
