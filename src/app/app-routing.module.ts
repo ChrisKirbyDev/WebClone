0150807d73e78e11fe3b50f22442b6df6d7edc55
Chris <chriskirbydev@gmail.com>
2020-10-22 09:05:14 +1300

    remove header html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { IndexTwoComponent } from './component/index-two/index-two.component';
 import { IndexThreeComponent } from './component/index-three/index-three.component';
 import { IndexFourComponent } from './component/index-four/index-four.component';
 import { IndexFiveComponent } from './component/index-five/index-five.component';
+import { AboutusComponent } from './component/aboutus/aboutus.component';
 
 const routes: Routes = [
-  { path: 'first-component', component: IndexComponent },
-  { path: 'second-component', component: IndexTwoComponent },
-  { path: 'third-component', component: IndexThreeComponent },
-  { path: 'fourth-component', component: IndexFourComponent },
-  { path: 'fifth-component', component: IndexFiveComponent },
+  { path: '', component: IndexComponent },
+  { path: 'first-component', component: IndexTwoComponent },
+  { path: 'second-component', component: IndexThreeComponent },
+  { path: 'third-component', component: IndexFourComponent },
+  { path: 'fourth-component', component: IndexFiveComponent },
+  { path: 'about-us-component', component: AboutusComponent },
 ];
 
 @NgModule({
