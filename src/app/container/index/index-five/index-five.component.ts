469d70315bb008283c6d400d7589bea7410317cb
Chris <chriskirbydev@gmail.com>
2020-10-23 20:04:45 +1300

    move into containers

---
+++ src/app/container/index/index-five/index-five.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-five',
+  templateUrl: './index-five.component.html',
+  styleUrls: ['./index-five.component.scss'],
+})
+export class IndexFiveComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
